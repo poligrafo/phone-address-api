a4009c16bf156da8f7aaa3252268dde34096aae3
poligrafo <poligrafo888@gmail.com>
2024-09-04 14:36:23 +0300

    features/add task 2 and edit README

--- app/services/phone_services.py
+++ app/services/phone_services.py
@@ -1,29 +1,10 @@
 import logging
-from typing import Optional
-
-from fastapi import HTTPException
 import redis.asyncio as redis
 
 from app.schemas.phone_address_schemas import PhoneAddress
 
 logger = logging.getLogger("app")
 
-
-# class PhoneService:
-#     def __init__(self, redis_client: redis.Redis) -> None:
-#         self.redis = redis_client
-#
-#     async def write_data(self, data: PhoneAddress) -> None:
-#         await self.redis.set(data.phone, data.address)
-#         logger.debug(f"Data written for phone: {data.phone}")
-#
-#     async def get_data(self, phone: str) -> Optional[str]:
-#         address = await self.redis.get(phone)
-#         if not address:
-#             logger.warning(f"Address not found for phone: {phone}")
-#             raise HTTPException(status_code=404, detail="Address not found")
-#         logger.debug(f"Data retrieved for phone: {phone}")
-#         return address.decode('utf-8')
 
 class PhoneService:
     def __init__(self, redis_client: redis.Redis, ttl: int = 3600):
